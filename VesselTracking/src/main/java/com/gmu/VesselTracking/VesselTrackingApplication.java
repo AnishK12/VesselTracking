24c3abb04a092a053530b2215166d709c1aa7dcc
AnishK12 <37880354+AnishK12@users.noreply.github.com>
2019-11-13 17:08:27 -0500

    Add files via upload

---
+++ VesselTracking/src/main/java/com/gmu/VesselTracking/VesselTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.gmu.VesselTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VesselTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VesselTrackingApplication.class, args);
+	}
+
+}
