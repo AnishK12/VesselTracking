0dbb45f9e75c136dbda66126a9c892948e7c2c4e
SabihM <48097948+SabihM@users.noreply.github.com>
2019-11-22 01:54:07 -0500

    Add files via upload

--- JsonSetup/ships.js
+++ JsonSetup/ships.js
@@ -1,5 +1,5 @@
 // The origional JS file that we pulled from the source
-const ships = require('./ships.json'); 
+const ships = require('./ships.json');
 
 // looked up a list of top 100 ports, used for start and end ports
 // https://sdwerecruit.co.uk/wp-content/uploads/2017/10/Lloyds-List-Top-100-Ports-2017-Report.pdf
@@ -8,10 +8,10 @@ const ports = require('./ports.json');
 // https://www.shiplilly.com/blog/the-most-common-types-of-cargo-ships/ list of a ship types
 const shipTypes = require('./ShipTypes.json');
 
-// https://transportgeography.org/?page_id=10258 lists the types of cargo 
+// https://transportgeography.org/?page_id=10258 lists the types of cargo
 const cargoTypes = require('./CargoTypes.json');
 
-// Gets value between min and max inclusive 
+// Gets value between min and max inclusive
 function getRandomPort(){
   min = 0
   max = 99
@@ -34,15 +34,21 @@ function getRandomCargo(){
 
 
 for (var ship in ships.features){
+  ships.features[ship].ID = ship;
   ships.features[ship].properties.startPort = ports[getRandomPort()];
   ships.features[ship].properties.endPort = ports[getRandomPort()];
   ships.features[ship].properties.cargo = cargoTypes[getRandomCargo()];
   ships.features[ship].properties.shipType = shipTypes[getRandomShipType()];
 }
 
+console.log(ships.features[23]);
+console.log(ships.features[99]);
+console.log(ships.features[123]);
+console.log(ships.features[1192]);
+
 var jsonData = JSON.stringify(ships);
 var fs = require('fs');
-fs.writeFile("database.json", jsonData, function(err) {
+fs.writeFile("test.json", jsonData, function(err) {
   if (err) {
       console.log(err);
   }
