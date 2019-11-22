ee8a115ea0721566dc449cd26320f4e45a2f38a8
SabihM <48097948+SabihM@users.noreply.github.com>
2019-11-22 01:49:31 -0500

    Add files via upload

---
+++ VesselTracking/src/main/java/com/gmu/VesselTracking/Model/UserData.java
@@ -0,0 +1,7 @@
+package com.gmu.VesselTracking.Model;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserData extends CrudRepository <User,Integer> {
+	
+}
--- VesselTracking/src/main/java/com/gmu/VesselTracking/VesselTrackingApplication.java
+++ VesselTracking/src/main/java/com/gmu/VesselTracking/VesselTrackingApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class VesselTrackingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VesselTrackingApplication.class, args);
-	}
+		
+	} 
 
 }
