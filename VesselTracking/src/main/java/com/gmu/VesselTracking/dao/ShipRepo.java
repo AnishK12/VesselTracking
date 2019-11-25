e1894b413a010e58bd8898788b761f6af8271791
zalzaman1 <30848276+zalzaman1@users.noreply.github.com>
2019-11-25 01:54:46 -0500

    Delete ShipRepo.java

--- VesselTracking/src/main/java/com/gmu/VesselTracking/dao/ShipRepo.java
+++
@@ -1,7 +0,0 @@
-package com.gmu.VesselTracking.dao;
-import org.springframework.data.repository.CrudRepository;
-import com.gmu.VesselTracking.Model.Ships;
-
-public interface ShipRepo extends CrudRepository<Ships, String>{
-	
-}
