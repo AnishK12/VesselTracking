badd8088ec3d084139f6592b0a7ccd29b828098b
AnishK12 <37880354+AnishK12@users.noreply.github.com>
2019-11-25 01:53:12 -0500

    Delete SearchHistoryRepo.java

--- src/main/java/com/gmu/VesselTracking/dao/SearchHistoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.gmu.VesselTracking.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gmu.VesselTracking.Model.SearchHistory;
-
-public interface SearchHistoryRepo extends JpaRepository<SearchHistory, Integer>{
-
-}
